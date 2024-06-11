d4a71f57961715cb866f7df52ccee3cbb742afda
stautuan <stautuan89@gmail.com>
2024-06-10 20:57:51 -0400

    Implemented the logic for a shootout scenario

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const countEl = document.querySelector('.count');
 const messageEl = document.querySelector('.message');
+const conditionEl = document.querySelector('.condition');
 
 function paces(seconds) {
   let count = 0;
@@ -15,8 +16,20 @@ function paces(seconds) {
 
       const randomDelay = Math.floor(Math.random() * 5 + 1) * 1000;
       console.log(randomDelay);
+
       setTimeout(() => {
         messageEl.innerHTML = 'He draws.....';
+
+        const defeat = setTimeout(() => {
+          conditionEl.innerHTML = 'And shoots. You are dead.';
+          window.removeEventListener('keydown', victory);
+        }, 500); // time limit to press a key
+
+        function victory() {
+          clearTimeout(defeat);
+          conditionEl.innerHTML = 'But you shoot first. You killed him.';
+        }
+        window.addEventListener('keydown', victory);
       }, randomDelay);
     }
   }, 1000);
