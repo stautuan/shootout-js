2a7fd17f2f7ecb588937a8942e9ae0477622c95f
stautuan <stautuan89@gmail.com>
2024-06-10 20:18:00 -0400

    Implemented countdown timer

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const countEl = document.querySelector('.count');
+
+function paces(seconds) {
+  let count = 0;
+
+  const timer = setInterval(() => {
+    count++;
+    countEl.innerHTML += `${count}...<br />`;
+
+    if (count >= seconds) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+paces(10);
